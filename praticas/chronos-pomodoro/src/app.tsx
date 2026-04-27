4657a630b6cfe14e23a08c27d71d21e93598e5db
Matheus Bruno <matheus.bruno@iesb.edu.br>
2026-04-27 20:31:10 -0300

    realizando aula prática021

--- praticas/chronos-pomodoro/src/app.tsx
+++ praticas/chronos-pomodoro/src/app.tsx
@@ -1,14 +1,73 @@
-// 1º: Importamos as variáveis (o tema)
+import { useState } from 'react';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 import './styles/theme.css';
-// 2º: Importamos os estilos globais
 import './styles/global.css';
-
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 import { Heading } from './components/Heading';
 
 export function App() {
+  const [numero, setNumero] = useState(0);
+
+  function handleClick() {
+    setNumero((prev) => prev + 1);
+    console.log(numero, Date.now());
+  }
+
   return (
     <>
-      <Heading />
+      <Heading>
+        Número: <span>{numero}</span>
+      </Heading>
+
+      <button onClick={handleClick}>Aumenta</button>
+
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <DefaultInput
+              labelText={numero.toString()}
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <Footer />
+      </Container>
     </>
   );
 }
